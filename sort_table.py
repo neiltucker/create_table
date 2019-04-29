2f6eff03a205979da0bda2ae929e98fb02b321f0
neiltucker <49943805+neiltucker@users.noreply.github.com>
2019-04-29 18:43:14 +0300

    Add files via upload

--- sort_table.py
+++ sort_table.py
@@ -2,11 +2,11 @@
 # The dataframe will then be sorted by first and lastname
 # The employeestable spreadsheet or CSV file must be in the current directory for this script to work
 import pandas as pd
-employees_df = pd.read_excel('employeestable.xls') 
-# employees_df = pd.read_csv('employeestable.csv')   # Uncomment this line to use a csv instead of a spreadsheet
+employees_df = pd.read_csv('employeestable.csv')       # Use a CSV file for the dataframe
+# employees_df = pd.read_excel('employeestable.xls')   # Use a spreadsheet for the dataframe.
 # employees_df.head()   # Uncomment this line to see the first five rows of the dataframe
 namesort = employees_df.sort_values(['LastName','FirstName'],ascending=True)
 hiredatesort = employees_df.sort_values(['HireDate'],ascending=True)
-namesort
-hiredatesort
+namesort.head()
+hiredatesort.head()
 
